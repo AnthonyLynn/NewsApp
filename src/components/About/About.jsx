656dde72193141df9d2564da0b26fb1ebf2c8d61
Anthony <anthonywlynn2002@gmail.com>
2026-01-03 23:41:55 -0500

    fix: About the author details and profile image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,4 @@
-import profileImage from "../../images/Profile_Placeholder.jpg";
+import profileImage from "../../images/Profile.jpg";
 import "./About.css";
 
 function About() {
@@ -8,10 +8,11 @@ function About() {
       <div>
         <h2 className="about__header">About the author</h2>
         <p className="about__description">
-          This block describes the project author. Here you should indicate your
-          name, what you do, and which development technologies you know. You
-          can also talk about your experience with TripleTen, what you learned
-          there, and how you can help potential customers.
+          Anthony Lynn is a Software Engineer with a background in game design.
+          They use React, CSS, and HTML; though recently started to use
+          Tailwind. After their graduation from TripleTen, a coding boot camp,
+          they've helped build responsive user focused applications from Figma
+          mockups.
         </p>
       </div>
     </section>
